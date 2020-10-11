b6e2afe310b6a0ff51cc33683bfd318c52692e3b
Mateusz Choma <kontakt@coderoad.pl>
2020-10-11 21:08:04 +0200

    All counter properties tested

--- tests/Counter.test.js
+++ tests/Counter.test.js
@@ -1,5 +1,60 @@
-describe('business logic – increasing and decreasing', () => {
+import Counter from '../src/Counter'
+import DecreasingCounter from '../src/DecreasingCounter'
 
-    
+describe('Counter', () => {
+
+    describe('business logic – increasing and decreasing', () => {
+
+        it('Counter should be an ES6 class instance', () => {
+
+            expect(() => Counter()).toThrow('Cannot call a class as a function')
+
+        })
+
+
+        it('should have `number` property for storing data', () => {
+
+            const counter1 = new Counter('body')
+
+            expect(counter1.number).toBeDefined()
+
+        })
+        
+        it('should have `selector` property for storing passed', () => {
+
+            const selector = 'body'
+            const counter1 = new Counter(selector)
+
+            expect(counter1.selector).toBeDefined()
+            expect(counter1.selector).toBe(selector)
+
+        })
+        
+        it('should have `container` property for storing found HTML element', () => {
+
+            const counter1 = new Counter('body')
+
+            expect(counter1.container).toBeDefined()
+            expect(counter1.container).toBeInstanceOf(HTMLElement)
+            expect(counter1.container).toBeInstanceOf(HTMLBodyElement)
+
+        })
+
+    })
+
+})
+
+describe('DecreasingCounter', () => {
+
+    describe('business logic – increasing and decreasing', () => {
+
+        it('DecreasingCounter should be an ES6 class instance', () => {
+
+            expect(() => DecreasingCounter()).toThrow('Cannot call a class as a function')
+
+        })
+
+
+    })
 
 })
