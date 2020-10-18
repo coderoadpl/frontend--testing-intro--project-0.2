2f286357f3cd505020d167315bd978225f81bcab
Mateusz Choma <kontakt@coderoad.pl>
2020-10-18 14:25:55 +0200

    Counter DOM tests added

--- tests/CounterDOM.test.js
+++ tests/CounterDOM.test.js
@@ -52,6 +52,28 @@ describe('Counter', () => {
 
     })
 
+    it('should render increased by 3 value after 3 click', () => {
+
+        const { getH1, getButton } = intCounter()
+
+        getButton().click()
+        getButton().click()
+        getButton().click()
+
+        expect(getH1().innerText).toBe(3)
+
+    })
+
+    it('should render increased value after direct `.inc` call', () => {
+
+        const { getH1, counter } = intCounter()
+
+        counter.inc()
+
+        expect(getH1().innerText).toBe(1)
+
+    })
+
 })
 
 describe('DecreasingCounter', () => {
